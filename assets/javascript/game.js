3ab4b5b8d2ac072f7b685a4bb6fa769e539dd94f
ScottE <scotteratigan@gmail.com>
2018-09-30 15:18:36 -0700

    Game is functional, and runs as its own class. Todo: add visual and sound effects.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,60 @@ var instructionWindow = document.getElementById("instruction-area");
 var currentGameWindow = document.getElementById("game-area");
 var scoreWindow = document.getElementById("score-area");
 var guessedLettersWindow = document.getElementById("guessed-letters-area");
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 var psychicGame = {
-	"playerWins": 0,
-	"playerLosses": 0,
-	"numOfGuesses": 0,
-	"listOfGuesses": [],
-	"secretLetter": "null",
-	"maxGuesses": 9,
+	"alphabet": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 	"gameInProgress": false,
+	"listOfGuesses": [],
+	"maxGuesses": 9,
+	"numOfGuesses": 0,
+	"playerLosses": 0,
+	"playerWins": 0,
 	"playerWon": false,
+	"secretLetter": "null",
 
 	displayCurrentGuesses: function() {
 		if (psychicGame.gameInProgress) {
-			currentGameWindow.innerHTML = "<p>You have " + (psychicGame.maxGuesses - psychicGame.numOfGuesses) + " guesses to figure out my letter, or you lose.</p>";
+			currentGameWindow.innerHTML = "<p>You have " + (psychicGame.maxGuesses - psychicGame.numOfGuesses) + " guesses remaining to figure out my letter, or you lose.</p>";
 		}
 		else {
 			currentGameWindow.innerHTML = "";
 		}
 	},
 
+	displayGuessedLetters: function() {
+		if (psychicGame.gameInProgress) {
+			var guessedLettersString = "";
+			for(var i = 0; i < psychicGame.listOfGuesses.length; i++) {
+				guessedLettersString += psychicGame.listOfGuesses[i];
+				if (i + 1 < psychicGame.listOfGuesses.length) {
+					guessedLettersString += ", ";
+				}
+			}
+			guessedLettersWindow.innerHTML = "<p>You have guessed: " + guessedLettersString + ".";
+		}
+		else {
+			guessedLettersWindow.innerHTML = "";
+		}
+	},
+
 	displayScore: function() {
 		var scoreString = "Wins: " + psychicGame.playerWins + " | Losses: " + psychicGame.playerLosses;
 		scoreWindow.innerHTML = scoreString;
+	},
+
+	gameIsLost: function() {
+		psychicGame.gameInProgress = false;
+		instructionWindow.innerHTML = "<p>Sorry, you have lost the game. Press RETURN to start a new game.</p>";
+		psychicGame.playerLosses++;
+		psychicGame.displayScore();
+	},
+
+	gameIsWon: function() {
+		psychicGame.gameInProgress = false;
+		instructionWindow.innerHTML = "<p>Congratulations, " + psychicGame.secretLetter + " was my letter. Press RETURN to start a new game.</p>";
+		psychicGame.playerWins++;
+		psychicGame.displayScore();
 	},
 
 	initializeGame: function() {
@@ -39,59 +69,30 @@ var psychicGame = {
 		psychicGame.numOfGuesses = 0;
 		psychicGame.gameInProgress = true;
 		psychicGame.secretLetter = 'a'; // todo: make this random.
+		psychicGame.secretLetter = psychicGame.alphabet[Math.floor(Math.random() * psychicGame.alphabet.length)];
 		instructionWindow.innerHTML = "<p>I'm thinking of a letter from a to z.</p>";
+		psychicGame.listOfGuesses = [];
 		psychicGame.displayCurrentGuesses();
-	}
+	},
 
-};
-
-function displayGuessedLetters() {
-	if (psychicGame.gameInProgress) {
-		var guessedLettersString = "";
-		for(var i = 0; i < psychicGame.listOfGuesses.length; i++) {
-			guessedLettersString += psychicGame.listOfGuesses[i];
-			if (i + 1 < psychicGame.listOfGuesses.length) {
-				guessedLettersString += ", ";
+	userGuesses: function(guessLetter) { // todo: grab value from class instead of passing it as argument
+		if (guessLetter === psychicGame.secretLetter) {
+			psychicGame.gameIsWon();
+		}
+		else {
+			instructionWindow.innerHTML = "<p>Incorrect guess - " + guessLetter + " is not my letter.</p>";
+			if (psychicGame.listOfGuesses.indexOf(guessLetter) === -1) {
+				psychicGame.numOfGuesses++;
+				psychicGame.listOfGuesses.push(guessLetter); // count the guess if the letter wasn't already guessed.
+				psychicGame.displayGuessedLetters();
+			}
+			if (psychicGame.numOfGuesses === psychicGame.maxGuesses) {
+				psychicGame.gameIsLost();
 			}
 		}
-		guessedLettersWindow.innerHTML = "<p>You have guessed: " + guessedLettersString + ".";
+		psychicGame.displayCurrentGuesses();	
 	}
-	else {
-		guessedLettersWindow.innerHTML = "";
-	}
-}
-
-function gameIsWon() {
-	psychicGame.gameInProgress = false;
-	instructionWindow.innerHTML = "<p>Congratulations, you have guessed my letter. Press RETURN to start a new game.</p>";
-	psychicGame.playerWins++;
-	psychicGame.displayScore();
-}
-
-function gameIsLost() {
-	psychicGame.gameInProgress = false;
-	instructionWindow.innerHTML = "<p>Sorry, you have lost the game. Press RETURN to start a new game.</p>";
-	psychicGame.playerLosses++;
-	psychicGame.displayScore();
-}
-
-function userGuesses(guessLetter) {
-	if (guessLetter === psychicGame.secretLetter) {
-		gameIsWon();
-	}
-	else {
-		instructionWindow.innerHTML = "<p>Incorrect guess - " + guessLetter + " is not my letter.</p>";
-		if (psychicGame.listOfGuesses.indexOf(guessLetter) === -1) {
-			psychicGame.numOfGuesses++;
-			psychicGame.listOfGuesses.push(guessLetter); // count the guess if the letter wasn't already guessed.
-			displayGuessedLetters();
-		}
-		if (psychicGame.numOfGuesses === psychicGame.maxGuesses) {
-			gameIsLost();
-		}
-	}
-	psychicGame.displayCurrentGuesses();
-}
+};
 
 // Events
 window.onload = psychicGame.initializeGame();
@@ -101,7 +102,7 @@ document.onkeyup = function(event) {
 	if (userKeyPress == "Enter" && !psychicGame.gameInProgress) {
 		psychicGame.startNewGame();
 	}
-	else if (psychicGame.gameInProgress && alphabet.includes(userKeyPress)) {
-		userGuesses(userKeyPress);
+	else if (psychicGame.gameInProgress && psychicGame.alphabet.indexOf(userKeyPress) > -1) {
+		psychicGame.userGuesses(userKeyPress);
 	}
 }
